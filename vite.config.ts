32f2db89cba6e90cb0e89bf412c4f591639311af
Morgan Nicol <morganichoui@gmail.com>
2023-05-24 10:09:40 +0200

    nothing

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+// https://vitejs.dev/
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: { modules: { localsConvention: 'camelCase' } },
   server: {
     host: 'localhost',
-    port: 3945
+    port: 3945,
+    strictPort: true
   },
-
+  preview: {
+    host: 'localhost',
+    port: 8080,
+  },
+  build: {}
 });
